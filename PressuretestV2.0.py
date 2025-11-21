81c231319f6fdd8f2003c4b920aa893b69c3364e
Remischouwenaars <remi.schouwenaars@tanis.com>
2025-11-21 14:32:06 +0100

    Update PressuretestV4.1
    email en logo added

--- PressuretestV2.0.py
+++ PressuretestV2.0.py
@@ -17,7 +17,7 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle, 
 # PATHS (logo t.o.v. dit script)
 # ======================
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-LOGO_PATH = os.path.join(BASE_DIR, "assets", "logo.png")
+LOGO_PATH = os.path.join(BASE_DIR, "logo.png")
 
 # ======================
 # TRANSLATIONS
